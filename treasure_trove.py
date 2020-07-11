d61c8e52cc3dfa590613ca2eed40e4003ac7e0e4
Graham Paasch <grahampaasch@gmail.com>
2020-07-11 12:12:12 -0500

    remove need for script user input, the plan is to type this all out from memory first thing as soon as I can open notepad++ in the testing room, and then run it on a router, while I read over the test for the first time, and draw out any network diagrams that will help me avoid mistakes and confusion, also in this time, while the treasure trove is printing, I can flag down the proctor for anything I need

--- treasure_trove.py
+++ treasure_trove.py
@@ -1,16 +1,12 @@
 import subprocess
-import sys
-search_string = sys.argv[2].lower()
 subprocess.check_output('cli set cli screen-length 0', shell=True)
 output = subprocess.check_output('cli help apropos .', shell=True)
 output_list = output.split('\n')
-help_topics = [line for line in output_list if line[0:10] == "help topic"]
+help_topics = [line for line in output_list if line[0:10] == 'help topic']
 for help_topic in help_topics:
     try:
         help = subprocess.check_output("cli " + help_topic, shell=True)
     except:
         continue
-    if search_string in help.lower():
+    if 'example: ' in help.lower():
         print(help_topic)
-else:
-    print("Further doc not found!")
